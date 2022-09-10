74fc65ffec912467c133a8edc661667df8ae50e1
Vagner Lourenço <90052483+VagnerSenac@users.noreply.github.com>
2022-09-10 10:47:17 -0300

    sucos

--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -6,3 +6,15 @@ def index(request):
     
 def sucodelaranja(request):
     return render(request, 'sucodelaranja.html')
+
+def sucodelimao(request):
+    return render(request, 'sucodelimao.html')
+
+def sucodeabacaxi(request):
+    return render(request, 'sucodeabacaxi.html')
+
+def sucodemaca(request):
+    return render(request, 'sucodemaca.html')
+
+def sucodemelancia(request):
+    return render(request, 'sucodemelancia.html')
